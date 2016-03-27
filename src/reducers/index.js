534e1c8f0c06c8fd2e296d315581e5c0abeced04
dmoon <dmoon.t@gmail.com>
2016-03-27 12:05:54 +0800

    refactor reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
+import * as reducers from './registReducers';
 
 /**
  * combineReducers is important to understand. As your app might grow in size
@@ -14,8 +14,6 @@ import counter from './counter';
  *
  * More info: http://rackt.org/redux/docs/api/combineReducers.html
  */
-const rootReducer = combineReducers({
-  counter, // you might be used to: counter: counter,
-});
+const rootReducer = combineReducers(reducers);
 
 export default rootReducer;
